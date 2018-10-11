0c9ca09f8e16ecd0ca679620ab958a614da20157
Andres Bonilla <andresdb91@gmail.com>
2018-10-11 02:17:02 -0300

    Agrega cabeceras para servicio rabbit

--- rabbit/rabbit_service.py
+++ rabbit/rabbit_service.py
@@ -1,5 +1,39 @@
+import pika
+import threading
 
-class RabbitService:
 
-    def __init__(self):
-        pass
+def init():
+    init_auth_listeners()
+    init_order_listeners()
+
+
+def init_auth_listeners():
+    pass
+
+
+def init_order_listeners():
+    pass
+
+
+def listen_auth_logout():
+    pass
+
+
+def listen_order_cancel():
+    pass
+
+
+def listen_order_validate():
+    pass
+
+
+def send_transaction_validate():
+    pass
+
+
+def send_transaction_failed():
+    pass
+
+
+def send_service_restart():
+    pass
