ea0c325fee48a9ceaa1ef7de7053712e000a3504
Andres Bonilla <andresdb91@gmail.com>
2018-10-11 02:36:32 -0300

    Implementa documentos

--- wallet/documents.py
+++ wallet/documents.py
@@ -1,30 +1,30 @@
-from mongoengine import *
+from mongoengine import Document
+from mongoengine import fields
+from mongoengine import connect
+from datetime import datetime
 
 connect('ecommerce-wallet', host='localhost', port=27017)
 
 
 class Wallet(Document):
-    # wallet_id
-    # user_id
-    # balance
-    # balance_datetime
-    # status
-    # status_datetime
-    pass
+    wallet_id = fields.IntField(required=True)
+    user_id = fields.IntField(required=True)
+    balance = fields.DecimalField(required=True)
+    balance_datetime = fields.DateTimeField(default=datetime.now)
+    status = fields.StringField(required=True)
+    status_datetime = fields.DateTimeField(default=datetime.now)
 
 
 class Transaction(Document):
-    # transaction_id
-    # transaction_type
-    # wallet_src
-    # wallet_dst
-    # amount
-    # status
-    # status_datetime
-    pass
+    transaction_id = fields.IntField(required=True)
+    transaction_type = fields.StringField(required=True)
+    wallet_src = fields.IntField()
+    wallet_dst = fields.IntField()
+    amount = fields.DecimalField(required=True)
+    status = fields.StringField(required=True)
+    status_datetime = fields.DateTimeField(default=datetime.now)
 
 
 class ServiceStatus(Document):
-    # status
-    # status_datetime
-    pass
+    status = fields.StringField(required=True)
+    status_datetime = fields.DateTimeField(default=datetime.now)
