b519c95e77c667e826879c2f2e28453e6697d6c9
Andres Bonilla <andresdb91@gmail.com>
2018-11-17 19:59:31 -0300

    Implementa cambio de estado de una billetera

--- wallet/command_service.py
+++ wallet/command_service.py
@@ -1,6 +1,23 @@
+from datetime import datetime
+from utils.security import errors
+
+from wallet.documents import Transaction, Wallet
+
 
 def update_status(user_id, params):
-    pass
+
+    wallet = Wallet.objects(user_id=int(user_id))
+
+    if not wallet or len(wallet) > 1:
+        raise errors.InvalidArgument(user_id)
+
+    estado = params.get('estado')
+    if estado:
+        wallet.status = estado
+        wallet.status_datetime = datetime.now()
+        wallet.save()
+    else:
+        raise errors.InvalidArgument(params)
 
 
 def create_transaction(params):
