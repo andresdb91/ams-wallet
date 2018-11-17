c934a9b939ee366b4f2fd1437f096bc61bb57e8a
Andres Bonilla <andresdb91@gmail.com>
2018-11-17 19:13:17 -0300

    Agrega cabeceras para queries

---
+++ wallet/query_service.py
@@ -0,0 +1,23 @@
+
+def get_wallet_status(user_id):
+    pass
+
+
+def get_wallet_balance(user_id):
+    pass
+
+
+def check_available_amount(user_id, amount):
+    pass
+
+
+def get_wallet_recent_transactions(user_id):
+    pass
+
+
+def get_wallet_all_transactions(user_id):
+    pass
+
+
+def get_transaction(transaction_id):
+    pass
