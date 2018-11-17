d3207cea214c15237b1c18c350da1bcf4cfb872a
Andres Bonilla <andresdb91@gmail.com>
2018-11-17 17:55:29 -0300

    Agrega clases base y cabeceras de función a módulo de errores

---
+++ utils/errors.py
@@ -0,0 +1,82 @@
+import traceback
+import json
+
+
+class InvalidRequest(Exception):
+    def __init__(self, error):
+        self.error = error
+
+    def __str__(self):
+        return repr(self.error)
+
+
+class InvalidArgument(Exception):
+    def __init__(self, error):
+        self.error = error
+
+    def __str__(self):
+        return repr(self.error)
+
+
+class MultipleArgumentException(Exception):
+    def __init__(self, error):
+        self.error = error
+
+    def __str__(self):
+        return repr(self.error)
+
+
+class InvalidAuth(Exception):
+    def __init__(self, error):
+        self.error = error
+
+    def __str__(self):
+        return repr(self.error)
+
+
+class InvalidAccessLevel(Exception):
+    def __init__(self, error):
+        self.error = error
+
+    def __str__(self):
+        return repr(self.error)
+
+
+def handle_error(err):
+    if isinstance(err, InvalidRequest):
+        return handle_invalid_request(err)
+    elif isinstance(err, InvalidArgument):
+        return handle_invalid_argument(err)
+    elif isinstance(err, MultipleArgumentException):
+        return handle_multiple_argument_exception(err)
+    elif isinstance(err, InvalidAuth):
+        return handle_invalid_auth(err)
+    elif isinstance(err, InvalidAccessLevel):
+        return handle_invalid_access_level(err)
+    else:
+        traceback.print_exc()
+        return handle_unknown(err)
+
+
+def handle_invalid_request(err):
+    pass
+
+
+def handle_invalid_argument(err):
+    pass
+
+
+def handle_multiple_argument_exception(err):
+    pass
+
+
+def handle_invalid_auth(err):
+    pass
+
+
+def handle_invalid_access_level(err):
+    pass
+
+
+def handle_unknown(err):
+    pass
