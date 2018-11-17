0fed1039ce9ef0d3b59ab663f9b296fe7c0ddc8a
Andres Bonilla <andresdb91@gmail.com>
2018-11-17 17:38:31 -0300

    Comenta funcionalidades de ApiDoc

--- application/app.py
+++ application/app.py
@@ -1,4 +1,4 @@
-import os
+# import os
 
 import flask
 from flask_cors import CORS
@@ -11,26 +11,27 @@ from wallet import routes as wallet_routes
 class MainApp:
 
     def __init__(self):
-        self.flask_app = flask.Flask(__name__, static_folder='../public')
+        # self.flask_app = flask.Flask(__name__, static_folder='../public')
+        self.flask_app = flask.Flask(__name__)
         CORS(self.flask_app, support_credentials=True, automatic_options=True)
 
-        self._init_api_doc()
-        self._init_routes()
+        # self._init_api_doc()
+        # self._init_routes()
         self._init_rabbit()
         self._init_wallet()
 
-    def _init_api_doc(self):
-        os.system("apidoc -i ../ -o ./public")
-
-    def _init_routes(self):
-        # Servidor de archivos estáticos de apidoc
-        @self.flask_app.route('/<path:path>')
-        def api_index(path):
-            return flask.send_from_directory('../public', path)
-
-        @self.flask_app.route('/')
-        def index():
-            return flask.send_from_directory('../public', "index.html")
+    # def _init_api_doc(self):
+    #     os.system("apidoc -i ../ -o ../public")
+    #
+    # def _init_routes(self):
+    #     # Servidor de archivos estáticos de apidoc
+    #     @self.flask_app.route('/<path:path>')
+    #     def api_index(path):
+    #         return flask.send_from_directory('../public', path)
+    #
+    #     @self.flask_app.route('/')
+    #     def index():
+    #         return flask.send_from_directory('../public', "index.html")
 
     def _init_rabbit(self):
         rabbit_service.init()
