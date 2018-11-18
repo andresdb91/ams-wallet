8cf1b77c23e2a5715de142f2780bdbe9bdd127d4
Andres Bonilla <andresdb91@gmail.com>
2018-11-17 21:01:49 -0300

    Valida datos y crea transacción

--- wallet/command_service.py
+++ wallet/command_service.py
@@ -21,4 +21,30 @@ def update_status(user_id, params):
 
 
 def create_transaction(params):
-    pass
+
+    src_id = params.get('_id_orig')
+    dst_id = params.get('_id_dest')
+    amount = params.get('amount')
+    tr_type = params.get('type')
+
+    wallet_src = Wallet.objects(user_id=src_id)
+    if not wallet_src:
+        raise errors.InvalidArgument(src_id)
+
+    wallet_dst = (Wallet.objects(user_id=dst_id)
+                  or Wallet(user_id=dst_id, status='activa'))
+
+    if not amount:
+        raise errors.InvalidArgument(amount)
+
+    if not tr_type or tr_type not in [
+            'carga', 'debito', 'transferencia', 'consolidacion']:
+        raise errors.InvalidArgument(tr_type)
+
+    transaction = Transaction(
+        wallet_src=wallet_src,
+        wallet_dst=wallet_dst,
+        transaction_type=tr_type,
+        amount=amount)
+
+    transaction.save()
