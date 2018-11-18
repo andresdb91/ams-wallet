21f3802a79dc3e8fe91ee5d9ed80551b2966915a
Andres Bonilla <andresdb91@gmail.com>
2018-11-18 16:13:23 -0300

    Agrega conversiones de datos en comandos

--- wallet/command_service.py
+++ wallet/command_service.py
@@ -1,12 +1,13 @@
 from datetime import datetime
-from utils.security import errors
+from decimal import Decimal
 
+from utils.security import errors
 from wallet.documents import Transaction, Wallet
 
 
 def update_status(user_id, params):
 
-    wallet = Wallet.objects(user_id=int(user_id)).limit(1)
+    wallet = Wallet.objects(user_id=int(user_id)).first()
 
     if not wallet:
         wallet = Wallet(user_id=user_id, status='activa')
@@ -34,41 +35,41 @@ def create_transaction(params):
     amount = params.get('amount')
     tr_type = params.get('type')
 
-    wallet_src = Wallet.objects(user_id=src_id).limit(1)
-    wallet_dst = (Wallet.objects(user_id=dst_id).limit(1)
-                  or Wallet(user_id=dst_id, status='activa'))
+    if src_id:
+        wallet_src = Wallet.objects(user_id=src_id).first()
+
+    if dst_id:
+        wallet_dst = (Wallet.objects(user_id=dst_id).first()
+                      or Wallet(user_id=dst_id, status='activa'))
+
+    if not amount:
+        raise errors.InvalidArgument(amount)
+    else:
+        amount = Decimal(amount)
 
     if tr_type == 'carga':
-        if not amount:
-            raise errors.InvalidArgument(amount)
         transaction = Transaction(
-            wallet_dst=wallet_dst,
+            wallet_dst=wallet_dst.user_id,
             transaction_type=tr_type,
             amount=amount)
     elif tr_type == 'debito':
-        if not amount:
-            raise errors.InvalidArgument(amount)
         if not wallet_src:
             raise errors.InvalidArgument(src_id)
         transaction = Transaction(
-            wallet_src=wallet_src,
+            wallet_src=wallet_src.user_id,
             transaction_type=tr_type,
             amount=amount)
     elif tr_type == 'transferencia':
-        if not amount:
-            raise errors.InvalidArgument(amount)
         if not wallet_src:
             raise errors.InvalidArgument(src_id)
         transaction = Transaction(
-            wallet_src=wallet_src,
-            wallet_dst=wallet_dst,
+            wallet_src=wallet_src.user_id,
+            wallet_dst=wallet_dst.user_id,
             transaction_type=tr_type,
             amount=amount)
     elif tr_type == 'consolidacion':
-        if not amount:
-            raise errors.InvalidArgument(amount)
         transaction = Transaction(
-            wallet_dst=wallet_dst,
+            wallet_dst=wallet_dst.user_id,
             transaction_type=tr_type,
             amount=amount)
     else:
@@ -78,10 +79,10 @@ def create_transaction(params):
     wallet_dst.save()
 
     return {
-        '_id': transaction.transaction_id,
-        'created': transaction.transaction_dt,
-        '_id_orig': transaction.wallet_src,
-        '_id_dest': transaction.wallet_dst,
-        'amount': transaction.amount,
+        '_id': str(transaction._id),
+        'created': transaction.transaction_dt.strftime('%Y-%m-%d %H:%M:%S'),
+        '_id_orig': str(transaction.wallet_src),
+        '_id_dest': str(transaction.wallet_dst),
+        'amount': str(transaction.amount),
         'type': transaction.transaction_type
     }
