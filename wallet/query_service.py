c7972b6d8d86aa7e37a762d853462f85ae1c2880
Andres Bonilla <andresdb91@gmail.com>
2018-11-17 22:17:50 -0300

    Extrae funciones para obtener última consolidación y balance

--- wallet/query_service.py
+++ wallet/query_service.py
@@ -36,46 +36,8 @@ def get_wallet_balance(user_id):
             'dt_last_cons': None
         }
 
-    consolidation = Transaction.objects(
-        wallet_dst=wallet,
-        transaction_type='consolidacion'
-    ).order_by('-transaction_dt').first()
-
-    if consolidation:
-        cons_date = consolidation.transaction_dt
-        cons_id = consolidation.transaction_id
-    else:
-        cons_date = datetime.min
-        cons_id = None
-
-    transactions_from = Transaction.objects(
-        wallet_src=wallet,
-        transaction_dt__gt=cons_date
-    ).all()
-    transactions_to = Transaction.objects(
-        wallet_dst=wallet,
-        transaction_type='debito',
-        transaction_dt__gt=cons_date
-    ).all()
-
-    if consolidation:
-        balance = consolidation.amount
-    else:
-        balance = Decimal('0.00')
-
-    last_transaction = None
-
-    for tr_out in transactions_from:
-        balance -= tr_out.amount
-        if (last_transaction
-                and last_transaction.transaction_dt < tr_out.transaction_dt):
-            last_transaction = tr_out
-
-    for tr_in in transactions_to:
-        balance += tr_in.amount
-        if (last_transaction
-                and last_transaction.transaction_dt < tr_in.transaction_dt):
-            last_transaction = tr_in
+    cons_id, cons_date, cons_balance = get_last_consolidation_info(wallet)
+    balance, last_transaction = get_balance(wallet, cons_date, cons_balance)
 
     return {
         '_id': wallet.user_id,
@@ -101,3 +63,53 @@ def get_wallet_all_transactions(user_id):
 
 def get_transaction(transaction_id):
     pass
+
+
+def get_balance(wallet, cons_date, cons_balance):
+
+    transactions_from = Transaction.objects(
+        wallet_src=wallet,
+        transaction_dt__gt=cons_date
+    ).all()
+    transactions_to = Transaction.objects(
+        wallet_dst=wallet,
+        transaction_type='debito',
+        transaction_dt__gt=cons_date
+    ).all()
+
+    balance = cons_balance
+
+    last_transaction = None
+
+    for tr_out in transactions_from:
+        balance -= tr_out.amount
+        if (last_transaction
+                and last_transaction.transaction_dt < tr_out.transaction_dt):
+            last_transaction = tr_out
+
+    for tr_in in transactions_to:
+        balance += tr_in.amount
+        if (last_transaction
+                and last_transaction.transaction_dt < tr_in.transaction_dt):
+            last_transaction = tr_in
+
+    return balance, last_transaction
+
+
+def get_last_consolidation_info(wallet):
+
+    consolidation = Transaction.objects(
+        wallet_dst=wallet,
+        transaction_type='consolidacion'
+    ).order_by('-transaction_dt').first()
+
+    if consolidation:
+        cons_date = consolidation.transaction_dt
+        cons_id = consolidation.transaction_id
+        cons_balance = consolidation.amount
+    else:
+        cons_date = datetime.min
+        cons_id = None
+        cons_balance = Decimal('0.00')
+
+    return cons_id, cons_date, cons_balance
