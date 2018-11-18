ee27497857264693144c134d92a90f0aa816149a
Andres Bonilla <andresdb91@gmail.com>
2018-11-17 21:09:35 -0300

    Verifica y carga transacción según su tipo

--- wallet/command_service.py
+++ wallet/command_service.py
@@ -28,23 +28,43 @@ def create_transaction(params):
     tr_type = params.get('type')
 
     wallet_src = Wallet.objects(user_id=src_id)
-    if not wallet_src:
-        raise errors.InvalidArgument(src_id)
-
     wallet_dst = (Wallet.objects(user_id=dst_id)
                   or Wallet(user_id=dst_id, status='activa'))
 
-    if not amount:
-        raise errors.InvalidArgument(amount)
-
-    if not tr_type or tr_type not in [
-            'carga', 'debito', 'transferencia', 'consolidacion']:
+    if tr_type == 'carga':
+        if not amount:
+            raise errors.InvalidArgument(amount)
+        transaction = Transaction(
+            wallet_dst=wallet_dst,
+            transaction_type=tr_type,
+            amount=amount)
+    elif tr_type == 'debito':
+        if not amount:
+            raise errors.InvalidArgument(amount)
+        if not wallet_src:
+            raise errors.InvalidArgument(src_id)
+        transaction = Transaction(
+            wallet_src=wallet_src,
+            transaction_type=tr_type,
+            amount=amount)
+    elif tr_type == 'transferencia':
+        if not amount:
+            raise errors.InvalidArgument(amount)
+        if not wallet_src:
+            raise errors.InvalidArgument(src_id)
+        transaction = Transaction(
+            wallet_src=wallet_src,
+            wallet_dst=wallet_dst,
+            transaction_type=tr_type,
+            amount=amount)
+    elif tr_type == 'consolidacion':
+        if not amount:
+            raise errors.InvalidArgument(amount)
+        transaction = Transaction(
+            wallet_dst=wallet_dst,
+            transaction_type=tr_type,
+            amount=amount)
+    else:
         raise errors.InvalidArgument(tr_type)
-
-    transaction = Transaction(
-        wallet_src=wallet_src,
-        wallet_dst=wallet_dst,
-        transaction_type=tr_type,
-        amount=amount)
 
     transaction.save()
